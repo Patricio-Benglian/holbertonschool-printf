85d814b198081944d4b52e79ee50e0676548e632
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-28 09:27:23 -0700

    Added exit condition to get_conv

--- main.c
+++ main.c
@@ -16,5 +16,7 @@ int main(void)
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
+    _printf("%,");
+    _printf("%%%");
     return (0);
 }
